a678f2e9f2c14bfac0a2644861acab65e6cc602e
igor abramenkov <igorek699669@gmail.com>
2024-10-24 19:42:01 +0300

    add project with header and filters

---
+++ src/components/shared/filters.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import { Title } from "./title";
+import { FilterCheckbox } from "./filter-checkbox";
+import { Input } from "../ui/input";
+import { RangeSlider } from "./range-slider";
+import CheckboxFiltersGroup from "./checkbox-filters-group";
+import { text } from "stream/consumers";
+
+interface filtersProps {
+  className?: string;
+}
+
+const filters: FC<filtersProps> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Можно собирать" value="1" />
+        <FilterCheckbox text="Новинки" value="2" />
+      </div>
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={1000} />
+          <Input type="number" min={100} max={1000} placeholder="1000" />
+        </div>
+
+        <RangeSlider max={1000} min={100} step={1} />
+      </div>
+      <CheckboxFiltersGroup
+        title="Ингридиенты"
+        className="mt-5"
+        limit={6}
+        items={[
+          { text: "Сырный соус", value: "1" },
+          { text: "Моцарелла", value: "2" },
+          { text: "Чеснок", value: "3" },
+        ]}
+        defaultItems={[
+          { text: "Сырный соус", value: "1" },
+          { text: "Моцарелла", value: "2" },
+          { text: "Чеснок", value: "3" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default filters;
---
+++ src/components/shared/header.tsx
@@ -0,0 +1,53 @@
+import { cn } from "@/lib/utils";
+import { FC } from "react";
+import { Container } from "./container";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
+
+interface headerProps {
+  className?: string;
+}
+
+const header: FC<headerProps> = ({ className }) => {
+  return (
+    <header className={cn("border border-b", className)}>
+      <Container className="flex w-full items-center justify-between py-8">
+        <div className="flex items-center gap-4">
+          <Image src="/logo.png" alt="" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p
+              className="text-sm
+               text-gray-400 leading-3"
+            >
+              вкусней уже некуда
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button className={"flex items-center gap-1"} variant="outline">
+            <User size={16} />
+            Войти
+          </Button>
+          <div>
+            <Button className={cn("group relative", className)}>
+              <b>520 ₽</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart size={16} className="relative" strokeWidth={2} />
+                <b>0</b>
+              </div>
+              <ArrowRight
+                size={20}
+                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+              />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default header;
